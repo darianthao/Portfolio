f058e6b99191a32524714eb3354f777938e75517
darianthao <justdarian01@gmail.com>
2023-02-05 16:37:48 -0600

    animated header

--- src/components/TitleCard/TitleCard.js
+++ src/components/TitleCard/TitleCard.js
@@ -1,16 +1,25 @@
 import "./TitleCard.css"
+import {Button} from "react-bootstrap";
 
 function TitleCard() {
     return (
-        <div className="rainbow">
         <div className="Banner">
+                <ul className="circles">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+
             <h1 className="Title">
                 Darian Thao
             </h1>
             <p className="Caption">
                 Software Engineer | 1st Generation Hmong American
             </p>
-        </div>
+            <Button style={{marginTop: "10em"}}>Welcome to my Portfolio</Button>
+
         </div>
     );
 }
